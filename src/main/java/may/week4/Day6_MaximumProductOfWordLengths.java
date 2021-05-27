4d784a8d4ecb5c9f35c33e3d5cc1b58d3231d4b6
Amrendra Bagga <amrendrabagga@gmail.com>
2021-05-27 20:47:01 +0530

    [MAY-W4-D6] Maximum Product of Word Lengths

---
+++ src/main/java/may/week4/Day6_MaximumProductOfWordLengths.java
@@ -0,0 +1,22 @@
+package may.week4;
+
+public class Day6_MaximumProductOfWordLengths {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        int bitmask[] = new int[n];
+        for (int i=0; i<n; i++) {
+            for (char c : words[i].toCharArray()) {
+                bitmask[i] |= 1 << (c - 'a');
+            }
+        }
+        int res = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                if ((bitmask[i] & bitmask[j]) == 0) {
+                    res = Math.max(res, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return res;
+    }
+}
